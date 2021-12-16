eab6f412a59f8508e6fae3e9899b100d438c97b6
Hayley Gold <92735935+hayleyg523@users.noreply.github.com>
2021-12-16 10:00:29 -0500

    Creation of Site using JavaScript

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById('comingUpContent').style.display = 'none';
+document.getElementById('pastEventsContent').style.display = 'none';
+
+function hidePanels() {
+  document.getElementById('comingUpContent').style.display = 'none';
+  document.getElementById('pastEventsContent').style.display = 'none';
+  document.getElementById('eventsWeekContent').style.display = 'none';
+}
+
+function showComingUp() {
+  hidePanels();
+  document.getElementById('comingUpContent').style.display = 'block';
+}
+
+function showPastEvents() {
+  hidePanels();
+  document.getElementById('pastEventsContent').style.display = 'block';
+}
+
+function showEvents() {
+  hidePanels();
+  document.getElementById('eventsWeekContent').style.display = 'block';
+}
